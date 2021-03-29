78cbd7199b7c5965079a7b0e7fd29ed8f3f677cd
Lee Sharkey <leedsharkey@gmail.com>
2021-03-29 13:55:37 +0200

    Added updated plotting code for new variables

---
+++ plot_training_csv.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='args for plotting')
+    parser.add_argument(
+        '--datapath', type=str)
+    args = parser.parse_args()
+    return args
+
+def plot():
+    args = parse_args()
+
+    data = pd.read_csv(args.datapath + "/log.csv")
+    cols = list(data.columns)
+    # cols
+    cols.remove('wall_time')
+    cols.remove('num_episodes')
+    cols.remove('num_episodes_val')
+
+    cols.remove('max_episode_len')
+    cols.remove('min_episode_len')
+    cols.remove('val_max_episode_len')
+    cols.remove('val_min_episode_len')
+
+    cols.remove('max_episode_rewards')
+    cols.remove('min_episode_rewards')
+    cols.remove('val_max_episode_rewards')
+    cols.remove('val_min_episode_rewards')
+
+    cols.remove('timesteps')
+
+
+    plt.subplots(figsize=[10, 7])
+    for name in ['mean_episode_len', 'val_mean_episode_len']:
+        plt.plot(data['timesteps'], data[name]/100, label=name+"/100",
+                 alpha=0.8)
+    # cols.remove('max_episode_len')
+    cols.remove('mean_episode_len')
+    # cols.remove('min_episode_len')
+    # cols.remove('val_max_episode_len')
+    cols.remove('val_mean_episode_len')
+    # cols.remove('val_min_episode_len')
+
+    for name in cols:
+        plt.plot(data['timesteps'], data[name], label=name)
+    plt.xlabel('timesteps')
+    plt.legend()
+    plt.savefig(args.datapath + "/plot.png")
+
+if __name__ == '__main__':
+    plot()
