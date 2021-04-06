95859922c6acb16e9e56a4b38f0a5467ce02ff2f
Lee Sharkey <leedsharkey@gmail.com>
2021-04-06 16:15:24 +0200

    Changed summed loss to meaned loss

--- train_gen_model.py
+++ train_gen_model.py
@@ -258,9 +258,9 @@ def loss_function(preds, labels, mu, logvar, device):
             label = label / 255.
             loss = torch.abs(pred - label)
             loss = loss * mask
-            loss = torch.sum(loss)  # Mean Absolute Error
+            loss = torch.mean(loss)  # Mean Absolute Error
         else:
-            loss = torch.sum(torch.abs(pred - label))  # Mean Absolute Error
+            loss = torch.mean(torch.abs(pred - label))  # Mean Absolute Error
         #mse = F.mse_loss(pred, label) # TODO test whether MSE or MAbsE is better (I think the VQ-VAE2 paper suggested MAE was better)
         losses.append(loss)
 
