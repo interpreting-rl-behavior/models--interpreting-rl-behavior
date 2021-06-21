221a606a60777b308673e06c2de53c9b3463725e
Lee Sharkey <leedsharkey@gmail.com>
2021-06-21 21:52:55 +0100

    Changed plots so that losses for all variables are more similar and ignoring the first 100 updates

--- generative/plot_gen_model_training_csv.py
+++ generative/plot_gen_model_training_csv.py
@@ -33,18 +33,33 @@ def plot():
     cols.remove('batches')
     cols.remove('loss/total recon w/o KL')
 
+    # cols.remove('loss/act_log_probs')
+    # cols.remove('loss/KL')
+    # cols.remove('loss/reward')
+    # cols.remove('loss/hx')
+    # cols.remove('loss/done')
+    scales = {'loss/act_log_probs': 5e-3,
+              'loss/KL': 1e-2,
+              'loss/reward': 1e-2,
+              'loss/hx': 1e-1,
+              'loss/done': 1e-1,
+              'loss/obs': 1.}
+
     # Plot
     plt.subplots(figsize=[11, 11])
-    max_y = data[cols].max().max()  # largest value in any column
-    plt.yticks(np.arange(0, max_y, max_y/71))
+    max_y = data['loss/obs'][100:].max().max()  # largest value in any column
+    plt.yticks(np.arange(0, max_y, max_y/50))
 
     for name in cols:
-        plt.plot(data.index*10, data[name], label=name)
+        plt.plot(data.index[100:]*10, data[name][100:] * scales[name], label=name)
         plt.grid(b=True, which='both')
 
     plt.xlabel('# batches')
     plt.legend()
     plt.savefig(args.datapath + "/plot_gen_model_training.png")
+    data_name = args.datapath[-15:]
+    parent_dir = args.datapath[:100]
+    plt.savefig(parent_dir + f"plot_gen_model_training_{data_name}.png")
 
 if __name__ == '__main__':
     plot()
