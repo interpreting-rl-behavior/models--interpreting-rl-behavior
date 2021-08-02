76d3bc36f7e19b90085ea6b6f2c8995768fc1aaf
Lee Sharkey <leedsharkey@gmail.com>
2021-08-02 20:37:03 +0200

    Added PCA initialisation to tSNE for better global distance preservation and cleaned up imports

--- analysis/precomput_analysis_funcs.py
+++ analysis/precomput_analysis_funcs.py
@@ -7,13 +7,7 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.neighbors import kneighbors_graph
 #import umap
 from sklearn.preprocessing import StandardScaler
-
-import argparse
-
-import matplotlib
 import matplotlib.pyplot as plt
-import os
-import time
 
 
 def scale_then_pca_then_save(data, num_pcs, save_path, aux_name1, aux_name2):
@@ -96,7 +90,8 @@ def tsne_after_pca(data, num_pcs, num_tsne_components, save_path, aux_name1,
     pca_for_tsne = PCA(n_components=num_pcs)
     pca_for_tsne = pca_for_tsne.fit_transform(data)
     data_tsneed = TSNE(n_components=num_tsne_components,
-                      random_state=seed).fit_transform(pca_for_tsne)
+                      random_state=seed,
+                      init=pca_for_tsne[:,0:2]).fit_transform(pca_for_tsne)
     np.save(save_path + f'tsne_{aux_name1}_{aux_name2}.npy', data_tsneed)
 
 def nmf_then_save(data, num_factors, save_path, aux_name1, aux_name2):
