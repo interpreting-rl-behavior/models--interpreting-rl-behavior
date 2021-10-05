45ecb6370bdf3230c9a960112f92d5b0b9c003f8
Lee Sharkey <leedsharkey@gmail.com>
2021-10-05 22:43:54 +0200

    Forgot to add this util/parallel.py script for dataparallelism

---
+++ util/parallel.py
@@ -0,0 +1,9 @@
+import torch
+
+
+class DataParallel(torch.nn.DataParallel):
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            return getattr(self.module, name)
