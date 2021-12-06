da88f767137f92cfc78947fbb07208cd8fc23e78
Lee Sharkey <leedsharkey@gmail.com>
2021-12-06 14:37:48 +0100

    Fixed plotting for new RSSM logs

--- generative/plot_gen_model_training_csv.py
+++ generative/plot_gen_model_training_csv.py
@@ -45,27 +45,14 @@ def plot(data_path):
     cols = list(data.columns)
     cols.remove('epoch')
     cols.remove('batches')
-    cols.remove('loss/total recon w/o KL')
-
-    # cols.remove('loss/act_log_probs')
-    # cols.remove('loss/KL')
-    # cols.remove('loss/reward')
-    # cols.remove('loss/hx')
-    # cols.remove('loss/done')
-    scales = {'loss/act_log_probs': 5e-3,
-              'loss/KL': 1e-2,
-              'loss/reward': 1e-2,
-              'loss/hx': 1e-1,
-              'loss/done': 1e-1,
-              'loss/obs': 1.}
 
     # Plot
     plt.subplots(figsize=[11, 11])
-    max_y = data['loss/hx'][100:].max().max()  # largest value in any column
-    plt.yticks(np.arange(0, max_y, max_y/50))
+    # max_y = data['loss/hx'][100:].max().max()  # largest value in any column
+    # plt.yticks(np.arange(0, max_y, max_y/50))
 
     for name in cols:
-        plt.plot(data.index[100:]*10, data[name][100:] * scales[name], label=name)
+        plt.plot(data.index[10:]*10, data[name][10:], label=name)
         plt.grid(b=True, which='both')
 
     plt.xlabel('# batches')
