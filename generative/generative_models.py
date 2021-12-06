Major commit. Aligned timesteps in record.py, procgen_dataset.py, and elsewhere. All aligned with Deletang diagram. Also: fixed agent masking; uncommented agent_h loss; cleaned train_gen_model; added done and rew overlay to vids

generative/generative_models.py
@@ -42,16 +42,17 @@ class AgentEnvironmentSimulator(nn.Module):
 
         # Get input data for generative model
         full_ims = data['ims']
-        true_agent_h0 = data['hx'][-self.num_sim_steps]
-        agent_h_labels = data['hx'][-self.num_sim_steps:]
-        true_actions_inds = data['action'][-self.num_sim_steps:]
+        true_agent_h0 = data['hx'][self.num_init_steps-1] # -1 because 1st sim step is last init step
+        agent_h_labels = data['hx'][self.num_init_steps-1:]
+        true_actions_inds = data['action'][self.num_init_steps-2:] # -2 because we have to use a_{t-1} in combo with env_t to get o_t
         true_actions_1hot = torch.nn.functional.one_hot(true_actions_inds.long(),
                                                   self.action_size)
         true_actions_1hot = true_actions_1hot.float()
 
-        reward_labels = data['reward'][-self.num_sim_steps:] # TODO get the right timesteps
-        terminal_labels = data['terminal'][-self.num_sim_steps:] # TODO get the right timesteps
-        terminal_labels = terminal_labels_to_mask(terminal_labels)
+        reward_labels = data['reward'][self.num_init_steps-1:]
+        terminal_labels = data['terminal'][self.num_init_steps-1:]
+        # terminal_labels = terminal_labels_to_mask(terminal_labels)
+
         # Initialize env@t=0 and agent_h@t=0 # Later on, we're going to have to extract the initial steps here in order to make a VAE encoder from the initializer
         B = full_ims.shape[1]
         init_ims = full_ims[0:self.num_init_steps]
@@ -85,7 +86,7 @@ class AgentEnvironmentSimulator(nn.Module):
             labels = {'image': im_labels[i],
                       'reward':reward_labels[i],
                       'terminal':terminal_labels[i],
-                      'agent_h':agent_h_labels[i]}
+                      'agent_h':agent_h_labels[i+1]} # +1 because ag_h_{t-1} is input to stepper and to agent, but it outputs ag_h_t(hat). We want the label to be ag_h_t.
 
             embed = embeds[i]
             if use_true_actions:
@@ -247,11 +248,12 @@ class AgentEnvStepper(nn.Module):
         # Then use ims and agent_h to step the agent forward and produce an action
         image_rec = image_rec.squeeze()
         image_rec = dclamp(image_rec, self.image_range_min, self.image_range_max)
+        no_masks = torch.zeros_like(labels['terminal'])
         pred_action, pred_action_logits, pred_value, agent_h = \
-            self.agent.predict_STE(image_rec, agent_h, labels['terminal'],
+            self.agent.predict_STE(image_rec, agent_h, no_masks,
                                    retain_grads=True) # Lee: I'm ignorant of whether the terminal-masks are doing anything potentially dangerous
         loss_reconstr_agent_h = self.agent_hx_loss(agent_h, labels['agent_h'], labels['terminal']) # TODO appropriate masking using terminal. We don't want agent hx to be trained after the episode is done
-        loss_reconstr = loss_reconstr # + loss_reconstr_agent_h # TODO try without this loss first because I'm not sure it's aligned or otherwise working properly
+        loss_reconstr = loss_reconstr + loss_reconstr_agent_h # TODO try without this loss first because I'm not sure it's aligned or otherwise working properly
         return (
             post_or_prior,    # tensor(B, 2*S)
             pred_action,
@@ -262,7 +264,7 @@ class AgentEnvStepper(nn.Module):
             tensors
         )
 
-    def agent_hx_loss(self, pred_agent_h, label_agent_h, mask):
+    def agent_hx_loss(self, pred_agent_h, label_agent_h, terminals):
         """Based on jurgisp's 'vecobs_decoder'. To be honest I don't understand
          why the std is pre-specified like this. But it's unlikely to be hugely
          important; the agent_h_loss is auxiliary."""
@@ -271,7 +273,7 @@ class AgentEnvStepper(nn.Module):
         p = D.Normal(loc=pred_agent_h, scale=torch.ones_like(pred_agent_h) * std)
         p = D.independent.Independent(p, 1)  # Makes p.logprob() sum over last dim
         loss = -p.log_prob(label_agent_h) * var
-        loss = loss * mask
+        loss = loss * terminal_labels_to_mask(terminals)
         loss = loss.unsqueeze(-1)
         return loss
 

generative/procgen_dataset.py
@@ -34,14 +34,15 @@ class ProcgenDataset(Dataset):
         self.data_keys = list(self.null_element.keys())
 
     def __len__(self):
-        # note: - self.seq_len to avoid sampling the end of the dataset where
-        # the remaining sequence is too short.
+        """ N.b. We subtract self.seq_len here to avoid sampling the end of the dataset where
+        # the remaining sequence is too short."""
         return len(self.idx_to_epi_table) - self.seq_len
 
 
     def __getitem__(self, idx):
-        """It's kind of complicated, but it's as simple as I could make it so
-        that it could fetch all the types of edge cases we should expect:
+        """This getter is kind of complicated, but it's as simple as I could
+        make it so that it could fetch all the types of edge cases we should
+        expect:
             - normal - init seq same as main seq and NOT close to end of epi
             - normal end - init seq same as main seq and epi ends during main seq
             - last epi - init seq or main seq extends beyond last epi
@@ -86,6 +87,7 @@ class ProcgenDataset(Dataset):
                 data['episode_step'].loc[data['global_step'] == \
                                          full_segment_init_global_step]
             full_segment_init_step = int(full_segment_init_step)
+            assert full_segment_init_step >= 0
             element_first_step = 0
 
         full_segment_len = sum(frames['episode'] == episode_number)
@@ -93,11 +95,12 @@ class ProcgenDataset(Dataset):
         element_last_step = element_first_step + full_segment_len
 
         ## Make data_dict and put the parts you want in the batch element.
-        data = data[self.data_keys[:4]]
-        data = data.to_numpy().T.tolist()
+        labels_done_rew_v_a = self.data_keys[:4]
+        data_np = data[labels_done_rew_v_a]
+        data_np = data_np.to_numpy().T.tolist()
         vec_list = [ims, hx, lp]
-        data.extend(vec_list)
-        data_dict = {k: v for k, v in zip(self.data_keys, data)}
+        data_np.extend(vec_list)
+        data_dict = {k: v for k, v in zip(self.data_keys, data_np)}
 
         ## Insert data into null vecs
         batch_ele = {'done': np.zeros(self.seq_len),
@@ -105,7 +108,7 @@ class ProcgenDataset(Dataset):
                      'value': np.zeros(self.seq_len),
                      'action': np.zeros(self.seq_len),
                      'ims': np.zeros((self.seq_len, self.ims_ch, self.ims_hw, self.ims_hw)),
-                     'hx': np.zeros((self.seq_len, self.hx_size)),
+                     'hx': np.zeros((self.seq_len+1, self.hx_size)),
                      'act_log_probs': np.zeros((self.seq_len, self.act_space_size)),
                      }
         for key, val in data_dict.items():
@@ -113,15 +116,20 @@ class ProcgenDataset(Dataset):
                                   element_last_step))) == \
                    len(list(range(full_segment_init_step,
                                   full_segment_last_step)))
-            data_array = np.array(data_dict[key][full_segment_init_step:
-                                                 full_segment_last_step])
+            if key == 'hx':
+                # We get an extra hx because we need hx_{t-1} as input
+                #  to the agent at t, but we need hx_t to compare with hx_t_hat.
+                data_array = np.array(data_dict[key][full_segment_init_step:
+                                                     full_segment_last_step+1])
+            else:
+                data_array = np.array(data_dict[key][full_segment_init_step:
+                                                     full_segment_last_step])
             if key == 'ims':
                 data_array = data_array / 255.
-                # TODO consider copying the last frame after 'done' instead
-                #  of leaving as all zeros.
-            batch_ele[key][element_first_step:element_last_step] = data_array
+            batch_ele[key][element_first_step:
+                           element_first_step+data_array.shape[0]] = data_array
 
-        # Rename done (from RL repo) to terminal (from rssm/gen_model repo)
+        # Rename 'done' (from RL repo) to 'terminal' (from rssm/gen_model repo)
         batch_ele['terminal'] = batch_ele.pop('done')
 
         return batch_ele

record.py
@@ -1,4 +1,16 @@
-"""Makes a dataset for the generative model."""
+"""Makes a dataset for the generative model.
+
+A note on timestep indexing and saving:
+
+Each timestep's variables consist of the same variables as in the Deletang
+diagram (augmented with rew and done info) and the PREVIOUS hidden state. We
+use the previous hx because that is what is input to the agent at this timestep.
+ But the agent uses the (current) hidden_state to produce the action. But we
+ still save the (current) hidden_state at the end. This means that we need to
+ be careful about indexing when visualising. The (current) hidden state should
+ be aligned with the action and the obs at the current timestep. Just be careful
+  when taking gradients that you know what you're actually taking grads wrt.
+"""
 import random
 
 import numpy as np
@@ -165,10 +177,12 @@ if __name__=='__main__':
 
     # Init agent and env
     obs = agent.env.reset()
-    hidden_state = np.stack(
+    hidden_state_prev = np.stack(
         [agent.policy.init_hx.clone().detach().cpu().numpy()] \
         * agent.n_envs)     # init with hx param
+    prev_act = np.ones(agent.n_envs) * 4  # Because in Coinrun, 4==null_action.
     done = np.zeros(agent.n_envs)
+    rew = np.zeros(agent.n_envs)
 
     # Timestep trackers
     global_steps = 0
@@ -190,8 +204,11 @@ if __name__=='__main__':
         agent.policy.eval()
 
         # Step agent and environment
-        act, log_prob_act, value, next_hidden_state = agent.predict_record(obs, hidden_state, done)
-        next_obs, rew, done, info = agent.env.step(act)
+        act, log_prob_act, value, hidden_state = agent.predict_record(obs, hidden_state_prev, done)
+        obs_next, rew_next, done_next, info = agent.env.step(act)
+        #if done, append the final hidden state (even though it's never input to the
+        # agent) and the last obs (in order to black it out, and it also is
+        # never input to agent)
 
         # Store variables
         for i in range(n_envs):
@@ -205,18 +222,17 @@ if __name__=='__main__':
                                      'action': act[i],
                                      }, ignore_index=True)
 
-            obs_lists[i].append(obs[i])
-            hx_lists[i].append(hidden_state[i])
-            logprob_lists[i].append(log_prob_act[i])
+            if done[i]:
+                    black_obs = np.zeros_like(obs[i])
+                    obs_lists[i].append(black_obs)
+                    hx_lists[i].append(hidden_state_prev[i])
+                    hx_lists[i].append(hidden_state[i])
+                    logprob_lists[i].append(log_prob_act[i])
+            else:
+                obs_lists[i].append(obs[i])
+                hx_lists[i].append(hidden_state_prev[i])
+                logprob_lists[i].append(log_prob_act[i])
 
-        # Increment for next step
-        obs = next_obs
-        hidden_state = next_hidden_state
-        global_steps += 1
-        episode_steps += 1
-        print("Episode number: ", episode_number)
-        print("Episode len: ", episode_steps)
-        print("Done:        ", done + 0)
         # At end of episode
         if np.any(done):
             done_idxs = np.where(done)[0] # [0] is because np.where returns a tuple
@@ -224,7 +240,7 @@ if __name__=='__main__':
                 done_epi_idx = episode_number[idx]
 
                 if episode_number[idx] < max_episodes: # save episode len
-                    episode_lens[done_epi_idx] = episode_steps[idx]
+                    episode_lens[done_epi_idx] = episode_steps[idx] + 1
 
                 data[idx].to_csv(os.path.join(logdir,
                     f'data_gen_model_{done_epi_idx:05d}.csv'),
@@ -272,8 +288,20 @@ if __name__=='__main__':
                 logprob_lists[idx] = []
 
                 # Reset hidden state
-                hidden_state[idx,:] = np.stack(
+                hidden_state_prev[idx,:] = np.stack(
                     agent.policy.init_hx.clone().detach().cpu().numpy())
+
+
+        # Increment for next step
+        obs = obs_next
+        rew = rew_next
+        done = done_next
+        hidden_state_prev = hidden_state
+        global_steps += 1
+        episode_steps += 1
+        print("Episode number: ", episode_number)
+        print("Episode len: ", episode_steps)
+        print("Done:        ", done + 0)
 
         # End recording loop if max num recorded episodes OR time-limit has
         # been reached
@@ -295,12 +323,7 @@ if __name__=='__main__':
     print("Combining datasets")
     data = pd.DataFrame(columns=['global_step', 'episode'])
     list_of_ref_dfs = []
-    # for e in range(max_episodes):
-    #     epi_filename = os.path.join(logdir, f'data_gen_model_{e:05d}.csv')
-    #     data_e = pd.read_csv(epi_filename)
-    #     data = data.append(data_e[['global_step', 'episode']])
-    # data['global_step'] = np.arange(len(data['global_step'])) # Set global step
-    # # so that each data step has a unique global step.
+
     max_global_step = 0
     for e in range(max_episodes):
         print(f"Creating indices for episode {e}")

train_gen_model.py
@@ -12,7 +12,7 @@ import random
 import torch
 from generative.generative_models import AgentEnvironmentSimulator
 from generative.procgen_dataset import ProcgenDataset
-from overlay_image import overlay_actions
+from overlay_image import overlay_actions, overlay_box_var
 
 from collections import deque
 import torchvision.io as tvio
@@ -274,25 +274,23 @@ def train(epoch, args, train_loader, optimizer, gen_model, agent, logger, save_d
             logger.logkv('loss total', loss.item())
             logger.dumpkvs()
 
-        # Saving model
-        if batch_idx % args.save_interval == 0:
-            model_path = os.path.join(
-                save_dir,
-                'model_epoch{}_batch{}.pt'.format(epoch, batch_idx))
-            torch.save(
-                {'gen_model_state_dict': gen_model.state_dict(),
-                 'optimizer_state_dict': optimizer.state_dict()},
-                model_path)
-            logger.info('Generative model saved to {}'.format(model_path))
+        # # Saving model
+        # if batch_idx % args.save_interval == 0:
+        #     model_path = os.path.join(
+        #         save_dir,
+        #         'model_epoch{}_batch{}.pt'.format(epoch, batch_idx))
+        #     torch.save(
+        #         {'gen_model_state_dict': gen_model.state_dict(),
+        #          'optimizer_state_dict': optimizer.state_dict()},
+        #         model_path)
+        #     logger.info('Generative model saved to {}'.format(model_path))
 
         # Visualize the predictions compared with the ground truth
-        pred_images = torch.cat(
-            [tensors_list[t]['image_rec'] for t in range(args.num_sim_steps)],
-            dim=0)
+        pred_images, pred_terminals, pred_rews = extract_preds_from_tensors(args, tensors_list)
 
-        preds = {'ims': pred_images, 'actions': pred_actions_1hot}
+        preds = {'ims': pred_images, 'actions': pred_actions_1hot, 'terminals': pred_terminals, 'rews': pred_rews}
         if (epoch > 1 and batch_idx % 20000 == 0) or (epoch < 1 and batch_idx % 5000 == 0):
-        # if batch_idx % 10000 == 0 or (epoch < 1 and batch_idx % 20000 == 0):
+        # if batch_idx % 1 == 0 :
             visualize(args, epoch, train_loader, optimizer, gen_model,
                                logger, batch_idx=batch_idx, save_dir=save_dir,
                                device=device, data=data, preds=preds,
@@ -309,88 +307,6 @@ def train(epoch, args, train_loader, optimizer, gen_model, agent, logger, save_d
                                device=device, data=None, preds=None,
                                use_true_actions=False, save_root='demo_sim_acts')
 
-
-def compute_kl_div_categorical(p_logits, q_logits, num_categ_distribs=32, num_categs=32):
-
-    # episilon = 1e-7
-    ts = p_logits.shape[0]
-    b = p_logits.shape[1]
-    p_logits = p_logits.view(ts, b, num_categ_distribs, num_categs)
-    q_logits = q_logits.view(ts, b, num_categ_distribs, num_categs)
-
-    probs_p = torch.softmax(p_logits, dim=3)
-    probs_q = torch.softmax(q_logits, dim=3)
-    kl_div = torch.sum(probs_p * torch.log(probs_p / probs_q), dim=[0,2,3]) # sum over t and categoricals
-    kl_div = torch.mean(kl_div) # mean over batch
-    return kl_div
-
-def loss_function(args, preds, labels, train_info_bufs, device):
-
-    recon_loss_hyperparams = {'ims': args.loss_scale_ims,
-                              'hx': args.loss_scale_hx,
-                              'reward': args.loss_scale_reward,
-                              'terminal': args.loss_scale_terminal,
-                              'act_log_probs': args.loss_scale_act_log_probs
-                              }
-    KL_loss_hyperparams = {
-        'pp_KL': 1.0,
-        'KL_alpha': 0.8,
-    }
-
-    terminals = labels['terminal'][:, -args.num_sim_steps:]
-    before_terminals = terminal_labels_to_mask(terminals)
-
-    # Reconstruction loss
-    recon_losses = []
-    for key in recon_loss_hyperparams.keys():
-        pred  = torch.stack(preds[key], dim=1).squeeze()
-
-        label = labels[key].to(device).float().squeeze()
-        label = label[:, -args.num_sim_steps:]
-
-        # Calculate masks for hx & logprobs
-        if key in ['hx', 'act_log_probs']:
-            num_dims = len(label.shape) - 2
-            # subtract 2 is because batch and time dim are already present in the
-            # mask
-
-            unsqz_lastdim = lambda x: x.unsqueeze(dim=-1)
-            mask = before_terminals
-            for d in range(num_dims):
-                # Applies unsqueeze enough times to produce a tensor of the same
-                # order as the loss tensor. It can therefore be broadcast to the
-                # same shape as the loss tensor
-                mask = unsqz_lastdim(mask)
-
-            # Calculate loss
-            loss = (pred - label) * mask
-            loss = torch.mean(loss ** 2)
-        else:
-            # Calculate loss
-            loss = (pred - label)
-            loss = torch.mean(loss ** 2)
-
-        train_info_bufs[key].append(loss.item())
-        loss = loss * recon_loss_hyperparams[key]
-        recon_losses.append(loss)
-
-    recon_loss = sum(recon_losses)
-    train_info_bufs['total recon w/o KL'].append(recon_loss.item())
-
-    # KL term
-    alpha = KL_loss_hyperparams['KL_alpha']
-
-    q = preds['env_h_stoch_logits']  # approx posterior
-    p = preds['pred_env_h_stoch_logits']  # prior
-    q = torch.stack(q)
-    p = torch.stack(p)
-
-    kl_divergence_q = compute_kl_div_categorical(q.detach(), p) * alpha
-    kl_divergence_p = compute_kl_div_categorical(q, p.detach()) * (1-alpha)
-    kl_divergence = kl_divergence_q + kl_divergence_p
-    train_info_bufs['KL'].append(kl_divergence.item())
-
-    return recon_loss + kl_divergence, train_info_bufs
 
 def visualize(args, epoch, train_loader, optimizer, gen_model, logger,
               batch_idx, save_dir, device, data=None, preds=None, use_true_actions=True, save_root=''):
@@ -412,6 +328,8 @@ def visualize(args, epoch, train_loader, optimizer, gen_model, logger,
     # Get input data for generative model
     full_ims = data['ims']
     true_actions_inds = data['action'][-args.num_sim_steps:]
+    true_terminals = data['terminal'][-args.num_sim_steps:]
+    true_rews = data['reward'][-args.num_sim_steps:]
 
     # Forward pass to get predictions if not already done
     if preds is None:
@@ -423,11 +341,13 @@ def visualize(args, epoch, train_loader, optimizer, gen_model, logger,
                       imagine=True,
                       modal_sampling=True)
         # Extract predictions from model output
-        pred_images = torch.cat(
-            [tensors_list[t]['image_rec'] for t in range(args.num_sim_steps)],
-            dim=0)
+        pred_images, pred_terminals, pred_rews = extract_preds_from_tensors(args, tensors_list)
+
     else:
-        pred_images, pred_actions_1hot = preds['ims'], preds['actions']
+        pred_images = preds['ims']
+        pred_terminals = preds['terminals']
+        pred_rews = preds['rews']
+        pred_actions_1hot = preds['actions']
 
     # Establish the right settings for visualisation
     viz_batch_size = min(int(pred_images.shape[1]), 20)
@@ -449,10 +369,21 @@ def visualize(args, epoch, train_loader, optimizer, gen_model, logger,
         # (T,B,C,H,W) --> (B,T,H,W,C)
         pred_images = pred_images.permute(1,0,3,4,2)
         full_ims = full_ims.permute(1,0,3,4,2)
+        pred_terminals = pred_terminals.permute(1,0)
+        pred_rews = pred_rews.permute(1,0)
+        true_terminals = true_terminals.permute(1,0)
+        true_rews = true_rews.permute(1,0)
 
         for b in range(viz_batch_size):
             pred_im = pred_images[b]
             full_im = full_ims[b]
+            full_im = full_im[-args.num_sim_steps:]
+
+            # Overlay Done and Reward
+            pred_im = overlay_box_var(pred_im, pred_terminals[b], 'left')
+            pred_im = overlay_box_var(pred_im, pred_rews[b], 'right')
+            full_im = overlay_box_var(full_im, true_terminals[b], 'left')
+            full_im = overlay_box_var(full_im, true_rews[b], 'right')
 
             # Make predictions and ground truth into right format for
             #  video saving
@@ -465,8 +396,9 @@ def visualize(args, epoch, train_loader, optimizer, gen_model, logger,
             pred_im = overlay_actions(pred_im, viz_actions_inds[b], size=16)
 
             full_im = full_im.clone().detach().type(torch.uint8).cpu().numpy()
-            full_im = full_im[-args.num_sim_steps:]
+
             full_im = overlay_actions(full_im, true_actions_inds[b], size=16)
+
 
 
             # Join the prediction and the true image side-by-side
@@ -478,10 +410,17 @@ def visualize(args, epoch, train_loader, optimizer, gen_model, logger,
                        f'{epoch:02d}_{batch_idx:06d}_{b:03d}.mp4'
             tvio.write_video(save_str, combined_im, fps=14)
 
-
-def safe_mean(xs):
-    return np.nan if len(xs) == 0 else np.mean(xs)
-
+def extract_preds_from_tensors(args, tensors_list):
+    pred_images = torch.cat(
+        [tensors_list[t]['image_rec'] for t in range(args.num_sim_steps)],
+        dim=0)
+    pred_terminals = torch.cat(
+        [tensors_list[t]['terminal_rec'] for t in range(args.num_sim_steps)],
+        dim=0)
+    pred_rews = torch.cat(
+        [tensors_list[t]['reward_rec'] for t in range(args.num_sim_steps)],
+        dim=0)
+    return pred_images, pred_terminals, pred_rews
 
 
 class Namespace: