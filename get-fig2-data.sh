a74ca41d732774ad05fd799887ac8ca70a7ef27d
Lauro Langosco di Langosco <langosco.lauro@gmail.com>
2022-01-21 15:36:55 +0000

    get fig2 script to use multiple processes

--- get-fig2-data.sh
+++ get-fig2-data.sh
@@ -3,10 +3,14 @@ set -o errexit
 
 num_seeds=10000
 
-for dummy in {1..10}
-do
+run(){
     for random_percent in 0 1 2 3 6 11
     do
         python run_coinrun.py --model_file $random_percent --start_level_seed 0 --num_seeds $num_seeds --random_percent 100
     done
+}
+
+for dummy in {1..10}
+do
+    run &
 done
