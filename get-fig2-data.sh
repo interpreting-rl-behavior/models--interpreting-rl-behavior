a2bce5f8188fe815e078d8d290fe2313a96c5a5d
Lauro Langosco <langosco.lauro@gmail.com>
2022-01-21 16:41:52 +0000

    parallelize data collection with array jobs instead of spawning new processes

--- get-fig2-data.sh
+++ get-fig2-data.sh
@@ -6,11 +6,14 @@ num_seeds=10000
 run(){
     for random_percent in 0 1 2 3 6 11
     do
+	echo running model trained on random_percent $random_percent
         python run_coinrun.py --model_file $random_percent --start_level_seed 0 --num_seeds $num_seeds --random_percent 100
     done
 }
 
-for dummy in {1..10}
-do
-    run &
-done
+run
+
+# for dummy in {1..10}
+# do
+#     run &
+# done
