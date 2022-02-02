90008461614e3fab07c3f4d406419169187a0905
Lee Sharkey <leedsharkey@gmail.com>
2022-02-02 23:21:09 +0100

    Added a script to fix the data csv if some episodes accidentally get overwritten or something

---
+++ record_just_end.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+
+if __name__=='__main__':
+
+    # Make save dirs
+    logdir_base = "generative/"#'/cluster/scratch/sharkeyl/'##
+    logdir = os.path.join(logdir_base, 'data/')
+
+    # Making dataset for generative model
+    ## Init dataset
+    column_names = ['level_seed',
+                    'episode',
+                    'global_step',
+                    'episode_step',
+                    'done',
+                    'reward',
+                    'value',
+                    'action',]
+
+    print("Combining datasets")
+    data = pd.DataFrame(columns=column_names)
+    for e in range(70000):#7):#
+        epi_filename = logdir + f'data_gen_model_{e}.csv'
+        data_e = pd.read_csv(epi_filename)
+        data = data.append(data_e)
+        #os.remove(epi_filename)
+    data.to_csv(logdir + f'data_gen_model.csv',
+                index=False)
