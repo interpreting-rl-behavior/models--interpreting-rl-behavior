25cb33ae51ee1b1c5737a18393c8c5453b9aa6db
Lee Sharkey <leedsharkey@gmail.com>
2022-05-19 16:06:30 +0200

    Feat: Added methods for running xcorr plots for each cluster; changed code style to Black

--- xcorr_and_xcaus_plots.py
+++ xcorr_and_xcaus_plots.py
@@ -11,7 +11,7 @@ from dimred_projector import HiddenStateDimensionalityReducer
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
-class xplot_manager:
+class XplotManager:
     def __init__(self):
         args = self.parse_args()
         self.hp = hpf.load_interp_configs(args.interpreting_params_name)
@@ -21,7 +21,7 @@ class xplot_manager:
         )  # number of generated samples to use
         self.num_samples_hx_grad = 4000
         self.num_samples_env_h = self.hp.analysis.env_h.num_samples
-        self.num_samples_clusters = self.hp.analysis.jacobian.num_samples
+        self.num_samples_clusters = self.hp.analysis.combined_agent_env_hx.num_samples
         self.ts_per_sample = self.hp.analysis.saliency.num_sim_steps
         self.direction_type = self.hp.analysis.saliency.direction_type
         self.num_ica_components = self.hp.analysis.agent_h.n_components_ica
@@ -31,13 +31,17 @@ class xplot_manager:
         self.grad_projector = self.hx_projector.project_gradients
 
         hx_precomp_data_path = os.path.join("analysis", "hx_analysis_precomp")
-        clusters_precomp_data_path = os.path.join("analysis", "jacob_analysis_precomp")
+        # clusters_precomp_data_path = os.path.join("analysis", "jacob_analysis_precomp")
+        clusters_precomp_data_path = os.path.join(
+            "analysis", "combined_agent_env_hx_analysis_precomp"
+        )
+
         self.env_h_precomp_path = os.path.join("analysis", "env_analysis_precomp")
         env_components = np.load(
             f"{self.env_h_precomp_path}/pcomponents_env_{self.num_samples_env_h}.npy"
         )
         self.cluster_ids = np.load(
-            f"{clusters_precomp_data_path}/clusters_jacob_{self.num_samples_clusters}.npy"
+            f"{clusters_precomp_data_path}/clusters_per_sample_{self.num_samples_clusters}.npy"
         )
         self.hx_pc_components = np.load(
             f"{hx_precomp_data_path}/pcomponents_{self.num_samples_hx}.npy"
@@ -52,11 +56,15 @@ class xplot_manager:
         )
 
         # Prepare load and save dirs
-        self.generated_data_path = os.path.join(self.hp.generated_data_dir,
-                                                self.hp.analysis.agent_h.informed_or_random_init)
+        self.generated_data_path = os.path.join(
+            self.hp.generated_data_dir, self.hp.analysis.agent_h.informed_or_random_init
+        )
         save_path = self.hp.analysis.xplots.save_dir  # "xcorr_matrices/"
+        save_path_data = self.hp.analysis.xplots.save_dir_data
         self.save_path = os.path.join(os.getcwd(), "analysis", save_path)
         os.makedirs(self.save_path, exist_ok=True)
+        self.save_path_data = os.path.join(os.getcwd(), "analysis", save_path_data)
+        os.makedirs(self.save_path_data, exist_ok=True)
 
     def parse_args(self):
         parser = argparse.ArgumentParser(description="args for plotting")
@@ -88,23 +96,23 @@ class xplot_manager:
         - plots xcaus plot for that
         """
         hx_ics = self.gather_hx_ic_data()
-        extrema_types = ['high', 'low']
+        extrema_types = ["high", "low"]
         extrema_ts = [0, 2, 4]
         for direction_id in range(0, self.num_ica_components):
             for ext_ts in extrema_ts:
-                ext_ts_cond = hx_ics['timestep'] == ext_ts
+                ext_ts_cond = hx_ics["timestep"] == ext_ts
                 ts_sub_ext = ext_ts - self.hp.analysis.saliency.common_timesteps[0]
 
                 for extr_type in extrema_types:
                     # First get extrema samples for this d
                     extr_value = self.extrema_values[extr_type][direction_id]
-                    if extr_type == 'high':
+                    if extr_type == "high":
                         ic_cond = hx_ics[direction_id] > extr_value
-                    if extr_type == 'low':
+                    if extr_type == "low":
                         ic_cond = hx_ics[direction_id] < extr_value
 
                     combo_cond = ic_cond & ext_ts_cond
-                    sample_ids = list(hx_ics[combo_cond]['sample_id'])
+                    sample_ids = list(hx_ics[combo_cond]["sample_id"])
                     sample_ids = [int(id) for id in sample_ids]
 
                     # Get rid of outliers using cluster. First find clusters
@@ -113,29 +121,40 @@ class xplot_manager:
                     # strongly represented cluster
                     sample_cluster_ids = list(self.cluster_ids[sample_ids])
                     sample_cluster_ids_set = list(set(sample_cluster_ids))
-                    cluster_counts = np.array([sample_cluster_ids.count(i) for i in sample_cluster_ids_set])
-                    kept_cluster_ids_inds = (cluster_counts > 2).nonzero()[0] # two is an arbitrary threshold for outlier clusters
-                    kept_cluster_ids = [sample_cluster_ids_set[i] for i in kept_cluster_ids_inds]
-                    kept_sample_ids = [sample_ids[i] for i in range(len(sample_ids))
-                                            if sample_cluster_ids[i] in kept_cluster_ids]
+                    cluster_counts = np.array(
+                        [sample_cluster_ids.count(i) for i in sample_cluster_ids_set]
+                    )
+                    kept_cluster_ids_inds = (cluster_counts > 2).nonzero()[
+                        0
+                    ]  # two is an arbitrary threshold for outlier clusters
+                    kept_cluster_ids = [
+                        sample_cluster_ids_set[i] for i in kept_cluster_ids_inds
+                    ]
+                    kept_sample_ids = [
+                        sample_ids[i]
+                        for i in range(len(sample_ids))
+                        if sample_cluster_ids[i] in kept_cluster_ids
+                    ]
 
                     num_samples = len(kept_sample_ids)
-                    stacked_samples_grads = self.collect_grads(kept_sample_ids,
-                                                               standardize_scale_per_sample=False,
-                                                               clip_grads=False)
+                    stacked_samples_grads = self.collect_grads(
+                        kept_sample_ids,
+                        standardize_scale_per_sample=False,
+                        clip_grads=False,
+                    )
 
                     for ts in range(stacked_samples_grads.shape[0]):
                         print("Timestep: %i " % ts)
                         ts_slice = stacked_samples_grads[ts]
                         ts_sub = ts - self.hp.analysis.saliency.common_timesteps[0]
                         if ts_sub > 0:
-                            op_str = '+'
+                            op_str = "+"
                         else:
-                            op_str = ''
+                            op_str = ""
                         if ts_sub_ext >= 0:
-                            op_str_ext = '+'
+                            op_str_ext = "+"
                         else:
-                            op_str_ext = ''
+                            op_str_ext = ""
                         title_name = f"Cross-causation matrix: grads of hx_direction at t=0 (x-axis) \nwith respect to hx_directions at (t{op_str}{ts_sub}) (y-axis)\n Only samples where direction {direction_id} is extremely {extr_type} at t{op_str_ext}{ts_sub_ext} (N={num_samples} samples)\n in clusters {kept_cluster_ids}"
                         heatmap_name = f"xcaus_hx_t{op_str}{ts_sub}_d{direction_id}_{extr_type}_at_t{op_str_ext}{ts_sub_ext}"
                         self.plot_heatmap(
@@ -146,16 +165,14 @@ class xplot_manager:
                             clim=ts_slice.max() / 2,
                         )
 
-
     def gather_hx_ic_data(self):
         # Collect hx loadings
         hx_loadings = {}
         full_hx_loading_arrs = []
         for sample_id in range(self.num_samples_hx):
-            sample_id_str = f'sample_{sample_id:05d}'
+            sample_id_str = f"sample_{sample_id:05d}"
             hx = np.load(
-                os.path.join(self.generated_data_path,
-                             f"{sample_id_str}/agent_hs.npy")
+                os.path.join(self.generated_data_path, f"{sample_id_str}/agent_hs.npy")
             )
             hx_vecs = self.hx_projector.transform(hx)
             hx_vecs = hx_vecs.transpose()
@@ -176,7 +193,7 @@ class xplot_manager:
         }
 
         # Find the samples with extrema
-        col_names = ['sample_id', 'timestep']
+        col_names = ["sample_id", "timestep"]
         col_names.extend(list(range(0, self.num_ica_components)))
         flatten = lambda x: [item for sublist in x for item in sublist]
         ts_per_sample = self.hp.analysis.saliency.num_sim_steps
@@ -186,13 +203,83 @@ class xplot_manager:
         timesteps_vals = [list(range(0, ts_per_sample))] * self.num_samples_hx
         timesteps_vals = flatten(timesteps_vals)
         col_values = np.array([sample_ids, timesteps_vals])
-        col_values = np.concatenate([col_values.transpose(), full_hx_loading_array.transpose()], axis=1)
+        col_values = np.concatenate(
+            [col_values.transpose(), full_hx_loading_array.transpose()], axis=1
+        )
         hx_ics = pd.DataFrame(col_values, columns=col_names)
 
         return hx_ics
 
+    def plot_agent_hx_xcorrs_per_cluster(self):
+        # Get hx data (Cluster data has already been gotten)
 
-    def plot_direction_xcorrs_multi_timestep(self):
+        samples_data_hx = []
+        print("Collecting data together...")
+        for ep in range(0, self.num_samples_hx):
+            hx = np.load(
+                os.path.join(self.generated_data_path, f"sample_{ep:05d}/agent_hs.npy")
+            )
+            hx_vecs = self.hx_projector.transform(hx)
+            hx_vecs = hx_vecs.transpose()
+            samples_data_hx.append(hx_vecs)
+        samples_data_hx = np.stack(samples_data_hx)
+        print("Done collecting data together...")
+
+        # for each cluster...
+        clusters = list(set(self.cluster_ids))
+        for cluster in clusters:
+            print(f"Calculating cross correlation plots for cluster {cluster}")
+            # Filter hx data for that cluster
+            cluster_episode_ids = (self.cluster_ids == cluster).nonzero()[0]
+            cluster_episodes = samples_data_hx[cluster_episode_ids]
+            cluster_episodes = [
+                samples_data_hx[i] for i in range(cluster_episodes.shape[0])
+            ]
+
+            # Calc and plot xcorr plot for that cluster
+            self.plot_xcorrs_for_sample_group(cluster_episodes, cluster)
+
+        pass
+
+    def plot_xcorrs_for_sample_group(self, samples_data_hx, cluster):
+        K = 10
+        m = 1e3
+        num_samples = len(samples_data_hx)
+        for k in np.arange(0, K + 1, 1):
+            ## Collect together the pairs of vectors that we'll be using to make the xcorr matrix
+            xcorrs_hx = []
+            for hx_sample in samples_data_hx:
+                # Clip samples so pathologically large PC loadings don't have as much influence
+                hx_sample = np.clip(hx_sample, a_min=-m, a_max=m)
+                if k == 0:
+                    set_a_hx = hx_sample
+                    set_b_hx = hx_sample
+                else:
+                    set_a_hx = hx_sample[:, :-k]
+                    set_b_hx = hx_sample[:, k:]
+
+                ## Calculate the xcorr matrices
+                n = set_a_hx.shape[0]  # num elements in set
+                xcorr_hx = (set_a_hx @ set_b_hx.transpose()) / n
+
+                xcorrs_hx.append(xcorr_hx)
+
+            xcorrs_hx = np.stack(xcorrs_hx)
+            xcorrs_hx = np.mean(xcorrs_hx, axis=0)
+
+            ## Plot and save the xcorr matrices
+            print("Plotting for k=%i" % int(k))
+            plot_name = f"xcorr_hx_clust{cluster}_t+{int(k)}"
+            title_name = f"Cross-correlation matrix comparing t (y-axis) with t+{k} (x-axis) for cluster {cluster} (n={num_samples})"
+            self.plot_heatmap(
+                xcorrs_hx,
+                plot_name,
+                title_name,
+            )
+            array_savename = os.path.join(self.save_path_data, plot_name + '.npy')
+            np.save(array_savename, xcorrs_hx)
+
+    def plot_direction_xcorrs_multi_timestep_all_samples(self):
 
         # Collect env and hxs and put them into a list of arrays, where each
         # array is one sample.
@@ -282,7 +369,9 @@ class xplot_manager:
             )
             self.plot_heatmap(xcorrs_hx, "unordered_xcorr_hx_t+%i" % int(k), title_name)
 
-    def collect_grads(self, sample_ids, standardize_scale_per_sample=False, clip_grads=False):
+    def collect_grads(
+        self, sample_ids, standardize_scale_per_sample=False, clip_grads=False
+    ):
         # Get hidden states gradients that were produced by the generative model
         samples_dir_grads = []
 
@@ -315,15 +404,21 @@ class xplot_manager:
         if standardize_scale_per_sample:
             # per_sample AND per_timesteps
             shp = stacked_samples_grads.shape
-            reshaped_grads = stacked_samples_grads.reshape([shp[0], shp[1] * shp[2], shp[3]])
+            reshaped_grads = stacked_samples_grads.reshape(
+                [shp[0], shp[1] * shp[2], shp[3]]
+            )
             std_per_sample = reshaped_grads.std(axis=1)
             std_per_sample = std_per_sample.reshape(shp[0] * shp[3])
-            reshaped_grads = stacked_samples_grads.reshape(shp[0] * shp[3], shp[1] * shp[2])
-            std_per_sample = np.where(std_per_sample != 0,
-                                      std_per_sample,
-                                      np.ones_like(std_per_sample)) # make safe for div
+            reshaped_grads = stacked_samples_grads.reshape(
+                shp[0] * shp[3], shp[1] * shp[2]
+            )
+            std_per_sample = np.where(
+                std_per_sample != 0, std_per_sample, np.ones_like(std_per_sample)
+            )  # make safe for div
             reshaped_grads = reshaped_grads / (np.expand_dims(std_per_sample, axis=-1))
-            stacked_samples_grads = reshaped_grads.reshape(shp[0], shp[1], shp[2], shp[3])
+            stacked_samples_grads = reshaped_grads.reshape(
+                shp[0], shp[1], shp[2], shp[3]
+            )
 
         # Average over samples
         stacked_samples_grads = stacked_samples_grads.mean(-1)
@@ -331,7 +426,13 @@ class xplot_manager:
         return stacked_samples_grads
 
     def plot_heatmap(
-        self, matrix, name, plot_title, labels=None, clim=1.0, clim_low=None,
+        self,
+        matrix,
+        name,
+        plot_title,
+        labels=None,
+        clim=1.0,
+        clim_low=None,
     ):
 
         plt.rcParams["figure.figsize"] = (10, 10)
@@ -425,7 +526,9 @@ class xplot_manager:
         id_mat = np.eye(hx_dim, hx_dim)
         id_mat_mod = (id_mat * self.hx_projector.hx_std) + self.hx_projector.hx_mu
         ic_directions_in_hx = self.hx_projector.transform(id_mat_mod)
-        ic_directions_in_hx = ic_directions_in_hx / np.linalg.norm(ic_directions_in_hx, axis=0)
+        ic_directions_in_hx = ic_directions_in_hx / np.linalg.norm(
+            ic_directions_in_hx, axis=0
+        )
         num_ic_dirs = ic_directions_in_hx.shape[1]
         assert num_ic_dirs == self.hp.analysis.agent_h.n_components_ica
 
@@ -442,13 +545,14 @@ class xplot_manager:
             cosine_sim_mat,
             "Cosine_sim_between_IC_directions_in_hx_space",
             "Cosine similarity between IC directions in hx space",
-            clim=1.,
+            clim=1.0,
         )
 
 
 if __name__ == "__main__":
-    xpm = xplot_manager()
-    xpm.plot_extrema_xcaus_plots()
+    xpm = XplotManager()
+    xpm.plot_agent_hx_xcorrs_per_cluster()
+    # xpm.plot_extrema_xcaus_plots()
     # xcm.plot_direction_xcausation_multi_timestep()
-    # xcm.plot_direction_xcorrs_multi_timestep()
-    xpm.plot_cosine_sim_heatmap()
+    # xcm.plot_direction_xcorrs_multi_timestep_all_samples()
+    # xpm.plot_cosine_sim_heatmap()
